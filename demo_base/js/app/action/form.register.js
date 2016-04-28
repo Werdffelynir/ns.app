d9fa52e6e8787cc8431422d7ef35caba1ef6fcf0
werdffelynir <werdffelynir@gmail.com>
2016-04-28 20:18:15 +0300

    ns

---
+++ demo_base/js/app/action/form.register.js
@@ -0,0 +1,27 @@
+
+/**
+ * Action form.register.js
+ * @namespace App.Action.FormRegister
+ */
+
+(function(App, Dom, Tpl){
+
+    "use strict";
+
+
+    /**
+     * Register action namespace
+     * Using depending on the base application
+     */
+    var o = App.namespace('Action.FormRegister');
+
+    /**
+     * Construct for action
+     */
+    o.init = function() {
+
+    };
+
+
+
+})(App, Dom, Tpl);
---
+++ demo_base/js/app/controller/possessing.js
@@ -0,0 +1,37 @@
+/**
+ * Controller possessing.js
+ * @namespace App.Controller.Possessing
+ */
+
+(function(App, Dom, Tpl){
+
+    "use strict";
+
+
+    /**
+     * Register controller
+     * Using depending on the base application
+     */
+    var o = App.namespace('Controller.Possessing');
+
+    /**
+     * Construct call first when this controller run
+     */
+    o.construct = function() {
+
+        /**
+         * First we need to select all the elements necessary for work.
+         * But after the DOM is loaded
+         */
+        Dom.loaded(documentLoaded);
+    };
+
+    function documentLoaded(){
+
+        console.log('Controller possessing.js is run...');
+
+    }
+
+
+
+})(App, Dom, Tpl);
